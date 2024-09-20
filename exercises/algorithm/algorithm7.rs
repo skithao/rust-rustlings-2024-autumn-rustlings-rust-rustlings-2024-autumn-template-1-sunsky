c5d637339af5ccfbd39bf85580bc6cb2f19bc792
skithao <13133213+welcome-sunsky-git@user.noreply.gitee.com>
2024-09-20 23:57:15 +0800

    t198

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -3,7 +3,6 @@
 	This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
 #[derive(Debug)]
 struct Stack<T> {
 	size: usize,
@@ -31,8 +30,13 @@ impl<T> Stack<T> {
 		self.size += 1;
 	}
 	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
+		if self.is_empty() {
+			return None;
+		}
+		else{
+			self.size -= 1;
+			self.data.pop()
+		}
 	}
 	fn peek(&self) -> Option<&T> {
 		if 0 == self.size {
@@ -101,8 +105,46 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 
 fn bracket_match(bracket: &str) -> bool
 {
-	//TODO
-	true
+	let mut stack = Stack::new();
+	for c in bracket.chars() {
+		match c {
+			'(' | '[' | '{' => {
+				stack.push(c);
+			},
+			')' => {
+				if let Some(top) = stack.pop() {
+					if top != '(' {
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			},
+			']' => {
+				if let Some(top) = stack.pop() {
+					if top != '[' {
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			},
+			'}' => {
+				if let Some(top) = stack.pop() {
+					if top != '{' {
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			},
+			_ => {}
+		}
+	}
+	return stack.is_empty();
 }
 
 #[cfg(test)]
--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,8 +1,7 @@
 /*
 	queue
-	This question requires you to use queues to implement the functionality of the stac
+	This question requires you to use queues to implement the functionality of the stack
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -54,28 +53,39 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
-	q1:Queue<T>,
-	q2:Queue<T>
+	q1: Queue<T>,
+	q2: Queue<T>,
 }
+
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            q1: Queue::<T>::new(),
+            q2: Queue::<T>::new(),
         }
     }
+
     pub fn push(&mut self, elem: T) {
-        //TODO
+        self.q2.enqueue(elem);
+        while !self.q1.is_empty() {
+            if let Ok(value) = self.q1.dequeue() {
+                self.q2.enqueue(value);
+            }
+        }
+
+        std::mem::swap(&mut self.q1, &mut self.q2);
     }
+
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.q1.is_empty() {
+            Err("Stack is empty")
+        } else {
+            self.q1.dequeue()
+        }
     }
+
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
@@ -84,7 +94,7 @@ mod tests {
 	use super::*;
 	
 	#[test]
-	fn test_queue(){
+	fn test_queue() {
 		let mut s = myStack::<i32>::new();
 		assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
